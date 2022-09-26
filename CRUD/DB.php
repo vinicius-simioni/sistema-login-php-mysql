fc12edf32888bdc23391f18326d9c65394f461b1
Vinícius Simioni <viniciussimioni@outlook.com>
2022-09-26 14:37:07 -0300

    add usuario existe

--- CRUD/DB.php
+++ CRUD/DB.php
@@ -1,7 +1,8 @@
 <?php
 require_once('config.php');
+require_once('Tools.php');
 
-class DB {
+class DB extends Tools{
     protected string $tabela = 'login-dados';
 
     function __construct (
@@ -11,20 +12,17 @@ class DB {
 
     public function insert() {
         $sql = "SELECT * FROM usuarios WHERE email = '$this->email' and senha = '$this->senha'";
+
         global $mysqli;
         $result = $mysqli->query($sql);
-        mysqli_query($mysqli, "INSERT INTO usuarios (email, senha) VALUES ('$this->email', '$this->senha')" );
-        echo "<p>Salvo com sucesso</p>";
 
-        // if(mysqli_num_rows($result) < 1){
-        //     unset($_SESSION['email']);
-        //     unset($_SESSION['senha']);
-        //     header('Location: login.php');
-        // } else {
-        //     $_SESSION['email'] = $email;
-        //     $_SESSION['senha'] = $senha;
-        //     header('Location: sistema.php');
-        // }
+        if(mysqli_num_rows($result) < 1){
+            mysqli_query($mysqli, "INSERT INTO usuarios (email, senha) VALUES ('$this->email', '$this->senha')" );
+            echo "<p>Salvo com sucesso</p>";
+        } else {
+            echo "<p>Usuário já existe</p>";
+        }
+
     }
 
 }
